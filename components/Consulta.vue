d639de4fa817eb421777af9151ae9cfe42b6f1ef
Breno Zukowski <brenozukotech@gmail.com>
2020-08-26 15:24:31 -0300

    Made the consultation bar responsive.

--- components/Consulta.vue
+++ components/Consulta.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="lg:flex lg:justify-center py-4 background-consulta">
-    <div class="lg:flex justify-center ">
-      <img src="../assets/img/place.png" alt="place icon">
-      <h1 class="mr-10 ml-2 ">Consulte sua cobertura</h1>
+    <div class="flex items-center lg:justify-center ml-6 mb-4">
+      <img src="../assets/img/place.png" class="hidden lg:inline-block" alt="place icon">
+      <h1 class="lg:mr-10 ml-2">Consulte sua cobertura</h1>
     </div>
-    <div class = "mr-20">
-      <input type="text" class ="lg:mr-4 rounded-lg py-2 px-2" placeholder="Digite seu CEP" />
-      <input type="text" class="rounded-lg py-2 px-2" placeholder="Digite seu telefone" />
+    <div class = "ml-6">
+      <input type="text" class ="lg:mr-4 mb-4 rounded-lg py-4 lg:px-2 px-12" placeholder="Digite seu CEP" />
+      <input type="text" class="rounded-lg lg:mr-12 mb-4 py-4 lg:px-2 px-12" placeholder="Digite seu telefone" />
+      <button class ="background-button py-4 px-16 ml-8 text-white  rounded-lg focus:outline-none focus:shadow-outline hover:bg-purple-700">Verificar</button>
     </div>
-    <button class ="background-button text-white py-2 px-16 rounded-lg focus:outline-none focus:shadow-outline hover:bg-purple-700">Verificar</button>
   </div>
 </template>
 
